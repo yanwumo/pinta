849ab0ecafd23ef8b706da58372e82315d3ee0b5
Ben Yan <yanwumo@outlook.com>
2020-07-10 13:39:24 -0700

    Support creating jobs from template, using system text editor.

--- pinta/cli/jobs/crud.py
+++ pinta/cli/jobs/crud.py
@@ -22,10 +22,64 @@ def get_job_by_id(id):
     print(json.dumps(response, indent=2))
 
 
+job_types = {'symmetric', 'ps-worker', 'mpi', 'image-builder'}
+job_samples = {
+    'symmetric': {
+        "name": "",
+        "description": "",
+        "image": "ubuntu:latest",
+        "from_private": False,
+        "working_dir": "/",
+        "command": 'echo "Hello, world!"; sleep 300',
+        "num_replicas": 3,
+        "ports": "2222",
+        "scheduled": True
+    },
+    'ps-worker': {
+        "name": "",
+        "description": "",
+        "image": "ubuntu:latest",
+        "from_private": False,
+        "working_dir": "/",
+        "ps_command": 'echo "Hello, PS!"; sleep 300',
+        "worker_command": 'echo "Hello, worker!"; sleep 300',
+        "num_ps": 1,
+        "num_workers": 2,
+        "ports": "2222",
+        "scheduled": True
+    },
+    'mpi': {
+        "name": "",
+        "description": "",
+        "image": "ubuntu:latest",
+        "from_private": False,
+        "working_dir": "/",
+        "master_command": 'echo "Hello, master!"; sleep 300',
+        "replica_command": 'echo "Hello, replica!"; sleep 300',
+        "num_replicas": 2,
+        "ports": "2222",
+        "scheduled": True
+    },
+    'image-builder': {
+        "name": "",
+        "description": "",
+        "from_image": "ubuntu:latest",
+        "scheduled": True
+    }
+}
+
+
 @click.command('job')
-@click.option('-f', '--file', required=True, type=click.File('r'))
-def create_job(file):
-    url = config.host + '/api/jobs/'
-    data = file.read()
+@click.option('-t', '--type', required=True, type=click.Choice(job_types, case_sensitive=False))
+@click.option('-f', '--file', type=click.File('r'))
+def create_job(type, file):
+    url = config.host + f'/api/jobs/{type}'
+    if file:
+        data = file.read()
+    else:
+        data = click.edit(json.dumps(job_samples[type], indent=4), extension='.json')
+        if data is None:
+            print('Exited from editor, job not created')
+            return
     response = requests.post(url, data=data, auth=BearerAuth()).json()
     print(json.dumps(response, indent=2))
